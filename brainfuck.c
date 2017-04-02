daef350299eba402c04b45011da65d15aad02bd7
wangle <048117wangle@gmail.com>
2017-04-03 01:07:41 +0800

    Finished C version

--- brainfuck.c
+++ brainfuck.c
@@ -1,33 +1,120 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
-#define STACK_DEPTH 1024
+#define CODE_LENGTH 4096
+#define STACK_FRAME 1024
+#define DATA_LENGTH 4096
+//code area
+char code[CODE_LENGTH];
+//stack frame (for nested loop)
+char* stack[STACK_FRAME];
+//runtime data
+char data[DATA_LENGTH];
+//code pointer
+char* cp=code;
+//stack pointer
+char** sp=stack;
+//data pointer
+char* dp=data;
+/*
+ * function mapping, inspired by a blog.
+ * key:instruction character, ASCII
+ * value:instruction function, function pointer
+ */
+typedef void (*INS)();
+/*
+ * according to ASCII table, ']' is the last character of brainfuck's
+ * reserved word and its value is 93, so, to save memory, 94 is enough.
+ */
+INS ins[94];
 
-char stack[STACK_DEPTH];
-
-char* ptr=stack;
-//right shift
+/*
+ * right shift
+ */
 void rShift(){
-    ptr++;
+    dp++;
 }
-//left shift
+/*
+ * left shift
+ */
 void lShift(){
-    ptr--;
+    dp--;
 }
-//increase 1
+/*
+ * increase one
+ */
 void increase(){
-    (*ptr)++;
+    (*dp)++;
 }
-//decrease 1
+/*
+ * decrease one
+ */
 void decrease(){
-    (*ptr)--;
+    (*dp)--;
 }
-//input
+/*
+ * input
+ */
 void input(){
-    scanf("%c",ptr);
+    *dp=getchar();
 }
-//output
+/*
+ * output
+ */
 void output(){
-    putchar(*ptr);
+    putchar(*dp);
+}
+/*
+ * begin loop
+ */
+void begin(){
+	if(*dp){
+		//save loop start position to stack frame
+		*sp++=cp-1;
+	}
+	else{
+		//skip to next related ]
+		int nested=1;
+		for(cp++;*cp&&nested;cp++){
+			if(*cp=='[') nested++;
+			if(*cp==']') nested--;
+		}
+	}
+}
+/*
+ *end loop
+ */
+void end(){
+	if(*dp) cp=*(--sp);
 }
 
-
+int main(int argc,char** argv){
+	if(argc!=2){
+		printf("Usage:%s sourceFile\n",argv[0]);
+	}
+	else{
+		freopen(argv[1], "r", stdin);
+		char c;
+		while((c = getchar())!= EOF){
+			if(strchr("<>+-[].,",c))
+				*cp++=c;
+			//ignore whitespace and unvalid character
+			else if(strchr(" \n\r\t",c))
+				continue;
+			else
+				printf("ignore unvalid character:%c\n",c);
+		}
+		//reset
+		cp=code;
+		ins['>']=rShift;
+		ins['<']=lShift;
+		ins['+']=increase;
+		ins['-']=decrease;
+		ins[',']=input;
+		ins['.']=output;
+		ins['[']=begin;
+		ins[']']=end;
+		while(*cp) ins[*cp++]();
+	}
+}
